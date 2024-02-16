7dfb6422e7cb2162f5f78c889268d15463c72835
ShehapAltahawy59 <shehapkhalil62@gmail.com>
2024-02-16 06:34:11 -0800

    edit

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -18,11 +18,11 @@ void bubble_sort(int *array, size_t size)
     for (j =0; j < size-i-1; j++)
     {
         
-        if(array[i]>array[i+1])
+        if(array[j]>array[j+1])
         {
-            swap = array[i];
-            array[i]=array[i+1];
-            array[i+1]=swap;
+            swap = array[j];
+            array[j]=array[j+1];
+            array[j+1]=swap;
             print_array(array, size);
         }
         
