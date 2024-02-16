cde1146005cb5e4d3c6b3604231f6cc3203231e9
ShehapAltahawy59 <shehapkhalil62@gmail.com>
2024-02-16 09:24:54 -0800

    edit

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -28,7 +28,8 @@ void swap(listint_t **list, listint_t *node1, listint_t *node2)
 void insertion_sort_list(listint_t **list)
 {
 	listint_t *current;
-        if (list == NULL || *list == NULL || (*list)->next == NULL)
+
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
 	current = (*list)->next;
 	while (current)
