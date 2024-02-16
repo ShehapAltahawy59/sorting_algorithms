4191984d4ec9a5c7e5e7bb95db626af90de72159
ShehapAltahawy59 <shehapkhalil62@gmail.com>
2024-02-16 07:21:45 -0800

    edit

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -9,21 +9,30 @@
 void selection_sort(int *array, size_t size)
 {
 	int swap;
+	int *min;
 	size_t i, j;
 
+	min = array[0];
 	if (array == NULL || size < 2)
 		return;
 	for (i = 0; i < size - 1 ; i++)
 	{
+		min = array + i;
 		for (j = i + 1; j < size; j++)
 		{
-			if (array[i] > array[j])
+
+			if (*min > array[j])
 			{
-				swap = array[i];
-				array[i] = array[j];
-				array[j] = swap;
-				print_array(array, size);
+				min = array + j;
 			}
+		}
+		if (min != array[i])
+		{
+				swap = array[i];
+				array[i] = *min;
+				*min = swap;
+				print_array(array, size);
+		}
 		}
 	}
 }
