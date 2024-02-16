be5e20d87d53f2c97a32ab32c9c9869637c79fb7
ShehapAltahawy59 <shehapkhalil62@gmail.com>
2024-02-16 07:08:26 -0800

    edit

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -15,7 +15,7 @@ void selection_sort(int *array, size_t size)
 		return;
 	for (i = 0; i < size - 1 ; i++)
 	{
-		for (j = i; j < size; j++)
+		for (j = i + 1; j < size; j++)
 		{
 			if (array[i] > array[j])
 			{
